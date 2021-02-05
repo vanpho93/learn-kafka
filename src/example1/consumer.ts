0ada713efcb193a69b546484e0da10ffa1596054
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2021-02-05 15:20:00 +0700

    Test consumber

---
+++ src/example1/consumer.ts
@@ -0,0 +1,29 @@
+import * as _ from 'lodash'
+import { kafka } from './kafka'
+
+const emailConsumer1 = kafka.consumer({ groupId: 'email-sender-group' })
+const emailConsumer2 = kafka.consumer({ groupId: 'email-sender-group' })
+const brazeConsumer1 = kafka.consumer({ groupId: 'braze-notification-group' })
+const brazeConsumer2 = kafka.consumer({ groupId: 'braze-notification-group' })
+
+const consumers = [
+  emailConsumer1,
+  emailConsumer2,
+  brazeConsumer1,
+  brazeConsumer2,
+]
+
+_.forEach(consumers, (consumer, index) => {
+  consumer.subscribe({ topic: 'test-consumer-topic', fromBeginning: true })
+  consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        topic,
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+        consumerIndex: index,
+      })
+    }
+  })
+})
